19950be099c9ac4aec70a558894faa3efcf063b1
black <1358244533@qq.com>
2026-05-29 11:01:20 +0800

    fix font script path hypr -> niri

--- chezmoiscripts/run_once_after_install-fonts.sh
+++ chezmoiscripts/run_once_after_install-fonts.sh
@@ -6,9 +6,9 @@ if pacman -Q ttf-jetbrains-mono-nerd &>/dev/null; then
 fi
 echo ">>> 安装字体..."
 if command -v paru &>/dev/null; then
-    paru -S --needed --noconfirm $(cat ~/.config/hypr/fonts.txt)
+    paru -S --needed --noconfirm $(cat ~/.config/niri/fonts.txt)
 elif command -v yay &>/dev/null; then
-    yay -S --needed --noconfirm $(cat ~/.config/hypr/fonts.txt)
+    yay -S --needed --noconfirm $(cat ~/.config/niri/fonts.txt)
 else
-    echo "! 请手动安装字体: $(cat ~/.config/hypr/fonts.txt)"
+    echo "! 请手动安装字体: $(cat ~/.config/niri/fonts.txt)"
 fi
